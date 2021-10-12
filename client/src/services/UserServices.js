248f462f4da3396359bfae355139508ba5673830
lfoster1150 <lfoster1150@gmail.com>
2021-10-12 09:18:26 -0500

    Add user services

---
+++ client/src/services/UserServices.js
@@ -0,0 +1,28 @@
+import Client from './api'
+
+export const UpdateUser = async userId => {
+  try {
+    const res = await Client.put(`/user/update/${userId}`)
+    return res.data
+  } catch (error) {
+    throw error
+  }
+}
+
+export const DeleteUser = async userId => {
+  try {
+    const res = await Client.delete(`/user/delete/${userId}`)
+    return res.data
+  } catch (error) {
+    throw error
+  }
+}
+
+export const GetUser = async userId => {
+  try {
+    const res = await Client.get(`/user/${userId}`)
+    return res.data
+  } catch (error) {
+    throw error
+  }
+}
