f13e4c52565fe37d2e6c14748398fab0304cd7c0
lfoster1150 <lfoster1150@gmail.com>
2021-10-17 14:01:11 -0500

    Fixed services

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -5,7 +5,7 @@ import Axios from 'axios'
 const Client = Axios.create({
   baseURL:
     process.env.NODE_ENV === 'production'
-      ? process.env.REACT_APP_API_URL
+      ? process.env.VUE_APP_API_URL
       : 'http://localhost:5000/api'
 })
 
