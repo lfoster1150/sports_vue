453ff35f09ea764e42a0c248defdc5503f5f68e9
Luke Foster <lfoster1150@gmail.com>
2024-04-12 11:56:48 -0500

    Test

--- server/api/index.py
+++ server/api/index.py
@@ -42,20 +42,20 @@ def home():
     return 'Api up!'
 
 # Resources //
-api.add_resource(Login, '/api/auth/login')
-api.add_resource(Register, '/api/auth/register')
-api.add_resource(Session, '/api/auth/session')
-api.add_resource(UpdatePassword, '/api/auth/user/<int:user_id>')
-api.add_resource(UserTeams, '/api/user/teams')
-api.add_resource(UserPlayers, '/api/user/players')
-api.add_resource(UserById, '/api/user/<int:user_id>')
-api.add_resource(UserTeamsById, '/api/user/teams/<int:user_id>')
-api.add_resource(Teams, '/api/teams')
-api.add_resource(TeamDetails, '/api/teams/<int:team_id>')
-api.add_resource(TeamAPI, '/api/teamapi/<string:api_id>')
-api.add_resource(Players, '/api/players')
-api.add_resource(PlayerDetails, '/api/players/<int:player_id>')
-api.add_resource(PlayerAPI, '/api/playerapi/<string:api_id>')
+# api.add_resource(Login, '/api/auth/login')
+# api.add_resource(Register, '/api/auth/register')
+# api.add_resource(Session, '/api/auth/session')
+# api.add_resource(UpdatePassword, '/api/auth/user/<int:user_id>')
+# api.add_resource(UserTeams, '/api/user/teams')
+# api.add_resource(UserPlayers, '/api/user/players')
+# api.add_resource(UserById, '/api/user/<int:user_id>')
+# api.add_resource(UserTeamsById, '/api/user/teams/<int:user_id>')
+# api.add_resource(Teams, '/api/teams')
+# api.add_resource(TeamDetails, '/api/teams/<int:team_id>')
+# api.add_resource(TeamAPI, '/api/teamapi/<string:api_id>')
+# api.add_resource(Players, '/api/players')
+# api.add_resource(PlayerDetails, '/api/players/<int:player_id>')
+# api.add_resource(PlayerAPI, '/api/playerapi/<string:api_id>')
 
 
 if __name__ == '__main__':
