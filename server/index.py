e0f5f9f51dc1149ab441e8d8f0446743c3d3e802
Luke Foster <lfoster1150@gmail.com>
2024-04-12 11:26:48 -0500

    Change index location

--- server/index.py
+++ server/index.py
@@ -2,19 +2,19 @@ from flask import Flask
 from flask_restful import Api
 from flask_cors import CORS
 from flask_migrate import Migrate
-from .models.db import db
+from api.models.db import db
 import os
 
 ## import resources
-from .resources.user import UserTeams, UserTeamsById, UserById, UserPlayers
-from .resources.auth import Login, Register, Session, UpdatePassword
-from .resources.team import Teams, TeamDetails, TeamAPI
-from .resources.player import Players, PlayerDetails, PlayerAPI
-from .models.user import User
-from .models.team import Team
-from .models.player import Player
-from .models.user_team import user_teams
-from .models.user_player import user_players
+from api.resources.user import UserTeams, UserTeamsById, UserById, UserPlayers
+from api.resources.auth import Login, Register, Session, UpdatePassword
+from api.resources.team import Teams, TeamDetails, TeamAPI
+from api.resources.player import Players, PlayerDetails, PlayerAPI
+from api.models.user import User
+from api.models.team import Team
+from api.models.player import Player
+from api.models.user_team import user_teams
+from api.models.user_player import user_players
 
 
 app = Flask(__name__)
@@ -50,12 +50,12 @@ api.add_resource(UserTeams, '/api/user/teams')
 api.add_resource(UserPlayers, '/api/user/players')
 api.add_resource(UserById, '/api/user/<int:user_id>')
 api.add_resource(UserTeamsById, '/api/user/teams/<int:user_id>')
-# api.add_resource(Teams, '/api/teams')
-# api.add_resource(TeamDetails, '/api/teams/<int:team_id>')
-# api.add_resource(TeamAPI, '/api/teamapi/<string:api_id>')
-# api.add_resource(Players, '/api/players')
-# api.add_resource(PlayerDetails, '/api/players/<int:player_id>')
-# api.add_resource(PlayerAPI, '/api/playerapi/<string:api_id>')
+api.add_resource(Teams, '/api/teams')
+api.add_resource(TeamDetails, '/api/teams/<int:team_id>')
+api.add_resource(TeamAPI, '/api/teamapi/<string:api_id>')
+api.add_resource(Players, '/api/players')
+api.add_resource(PlayerDetails, '/api/players/<int:player_id>')
+api.add_resource(PlayerAPI, '/api/playerapi/<string:api_id>')
 
 
 if __name__ == '__main__':
